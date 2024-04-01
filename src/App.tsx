5870ee1aa58e5fbc7b334ebe6cc1238e9fc61eec
Raul Mogollon <berckbel@gmail.com>
2024-04-01 12:43:44 -0500

    navbar good and init pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './App.css'
+import { Route } from "wouter"
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { Navbar } from './components/Navbar'
+import { useUser } from './hooks/useUser'
 
 function App() {
 
+  const { isLogged } = useUser()
+
   return (
-      <h1>Berckbel Blog</h1>
+      <>
+      <Navbar />
+        <main className={"p-4 m-4 flex flex-col md:flex-row"}>
+          <Route component={Home} path={"/"} />
+          {!isLogged && <Route component={Login} path={"/login"} />}
+          {!isLogged && <Route component={Register} path={"/register"} />}
+        </main>
+      </>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Link } from "wouter"
+import { useUser } from "../hooks/useUser"
+
+export const Navbar = () => {
+
+    const { isLogged } = useUser()
+
+    return (
+        <>
+            <header className={"min-w-max bg-purple-900 flex flex-row justify-between items-center text-white text-3xl"}>
+                <div className={"flex p-3 ml-3"}>
+                    <Link to={"/"} className={"rounded-full p-2 hover:bg-purple-500"}>
+                        <h3>{"Home"}</h3>
+                    </Link>
+                </div>
+                <div className={"flex flex-col md:flex-row"}>
+                    {isLogged && <button className={"ml-auto mr-3 rounded-full p-2 hover:bg-purple-500"}>
+                        <h3>{"Logout"}</h3>
+                    </button>}
+                    {!isLogged && <Link to={"/login"} className={"ml-auto mr-3 rounded-full p-2 hover:bg-purple-500"}>
+                        <h3>{"Login"}</h3>
+                    </Link>}
+                    {!isLogged && <Link to={"/register"} className={"ml-auto mr-3 rounded-full p-2 hover:bg-purple-500"}>
+                        <h3>{"Register"}</h3>
+                    </Link>}
+                </div>
+            </header>
+        </>
+    )
+}
