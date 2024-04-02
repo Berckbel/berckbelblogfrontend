b4ca9336d89287109feb0bbf9e60607838fcb9b2
Raul Mogollon <berckbel@gmail.com>
2024-04-02 16:59:23 -0500

    post item ponter cursor and image min heigth

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,11 +1,11 @@
 export const PostItem = ({ post }: { post: Post }) => {
     return (
-        <div key={post.id}>
+        <div key={post.id} className={"cursor-pointer mb-10"}>
             <h1 className={"text-purple-600 text-center text-4xl font-bold"}>{post.title}</h1>
             <img
                 src={`${import.meta.env.VITE_BLOG_BACKEND_BASE_URL}${post.image_url}`}
                 alt={post.title}
-                className={"w-full h-[300px] object-cover"}
+                className={"w-full h-[350px] object-cover"}
             />
             <span className={"flex flex-row-reverse font-extrabold text-black"}>{post.created_at}</span>
         </div>
