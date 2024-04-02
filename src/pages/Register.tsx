517f02f7f77a0334777bce7f3ec91d21f6074c1f
Raul Mogollon <berckbel@gmail.com>
2024-04-02 13:26:08 -0500

    new min width

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -29,7 +29,7 @@ export const Register = () => {
             validationSchema={validationSchema}
             onSubmit={handleSubmit}
         >
-            <Form className={"flex flex-col min-w-96 text-3xl"}>
+            <Form className={"flex flex-col min-w-52 text-3xl"}>
                 <h3 className={"flex text-5xl justify-center mb-6 font-bold text-purple-900 p-2"}>
                     {"Register"}
                 </h3>
