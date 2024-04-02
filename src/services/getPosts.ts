1ab98f64696f156a3abadd43bfe60fa600020ce7
Raul Mogollon <berckbel@gmail.com>
2024-04-02 17:10:15 -0500

    get posts service updated in case access is not provided

--- src/services/getPosts.ts
+++ src/services/getPosts.ts
@@ -2,8 +2,9 @@ import axios from "axios"
 
 export const getPosts = ({ access }: { access: string } = { access: "" }): Promise<Post[]> => {
     const url: string = `${import.meta.env.VITE_BLOG_BACKEND_BASE_URL}/${import.meta.env.VITE_BLOG_API_PATH}`
+    const config = access ? { headers: { Authorization: `JWT ${access}` } } : {}
     return axios
-        .get<Post[]>(url, { headers: { Authorization: `JWT ${access}` } })
+        .get<Post[]>(url, config)
         .then((res) => {
             const posts: Post[] = res.data
             return posts
