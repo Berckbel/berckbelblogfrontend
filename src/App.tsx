26741cf1b0ba65c4488fd166c860d87c093e5ef1
Raul Mogollon <berckbel@gmail.com>
2024-04-11 15:27:36 -0500

    validate if the post exist before rendereing pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,20 @@ import { Post } from './pages/Post'
 import { PostCreate } from './pages/PostCreate'
 import { PostEdit } from './pages/PostEdit'
 import { Profile } from './pages/Profile'
+import { useBlog } from './hooks/useBlog'
 
 function App() {
 
   const { isLogged } = useUser()
+  const { existSelectedPost, existSelectedPostEdit } = useBlog()
 
   return (
       <>
       <Navbar />
         <main className={"p-4 m-16 flex flex-col justify-center"}>
           <Route component={Home} path={"/"} />
-          <Route component={Post} path={"/post"} />
-          {isLogged &&<Route component={PostEdit} path={"/post/edit"} />}
+          {existSelectedPost && <Route component={Post} path={"/post"} />}
+          {existSelectedPostEdit && isLogged &&<Route component={PostEdit} path={"/post/edit"} />}
           {isLogged &&<Route component={PostCreate} path={"/post/create"} />}
           {isLogged &&<Route component={Profile} path={"/profile"} />}
           {!isLogged && <Route component={Login} path={"/login"} />}
