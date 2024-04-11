a0139cce0b00d04c99270f1a4cc8b7a774b4eee5
Raul Mogollon <berckbel@gmail.com>
2024-04-11 08:15:04 -0500

    create comment only for logged users

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,13 +1,16 @@
 import { CommentForm } from "../components/Comment/CommentForm"
 import { CommentList } from "../components/Comment/CommentList"
 import { PostView } from "../components/Post/PostView"
+import { useUser } from "../hooks/useUser"
 
 export const Post = () => {
+
+    const { isLogged } = useUser()
 
     return (
         <>
             <PostView />
-            <CommentForm />
+            {isLogged && <CommentForm />}
             <CommentList />
         </>
     )
