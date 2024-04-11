7def953c22da169a57b1f17a2fb07d9e7e619cb0
Raul Mogollon <berckbel@gmail.com>
2024-04-11 08:43:18 -0500

    refactor getComments service

--- src/services/getComments.ts
+++ src/services/getComments.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getComments = ({ access, post_id }: { access?: string, post_id: number }): Promise<PostComment[]> => {
-    const url: string = `${import.meta.env.VITE_BLOG_BACKEND_BASE_URL}/${import.meta.env.VITE_BLOG_COMMENT_API_PATH}?post_id=${post_id}`
+    const url: string = `${import.meta.env.VITE_BLOG_BACKEND_BASE_URL}/${import.meta.env.VITE_BLOG_COMMENT_API_PATH}${post_id ? `?post_id=${post_id}` : ``}`
     const config = access ? { headers: { Authorization: `JWT ${access}` } } : {}
     return axios
         .get<PostComment[]>(url, config,)
